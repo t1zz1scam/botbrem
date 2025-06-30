deb528421841a529ff61e2d050174ef27d1fa935
t1zz1scam <y.selvin24@icloud.com>
2025-06-30 19:01:53 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -7,8 +7,8 @@ def main_menu(role="user"):
     return ReplyKeyboardMarkup(kb, resize_keyboard=True)
 
 admin_panel_kb = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton("📬 Заявки", callback_data="view_applications")],
-    [InlineKeyboardButton("💸 Выплаты", callback_data="manage_payouts")],
-    [InlineKeyboardButton("📰 Пост в канал", callback_data="post_to_channels")],
-    [InlineKeyboardButton("📊 Статистика", callback_data="view_stats")],
+    [InlineKeyboardButton(text="📬 Заявки", callback_data="view_applications")],
+    [InlineKeyboardButton(text="💸 Выплаты", callback_data="manage_payouts")],
+    [InlineKeyboardButton(text="📰 Пост в канал", callback_data="post_to_channels")],
+    [InlineKeyboardButton(text="📊 Статистика", callback_data="view_stats")],
 ])
