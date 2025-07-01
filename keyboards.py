bb3f84471a7e31b55ce62a67fe124b23e1e8f32b
t1zz1scam <y.selvin24@icloud.com>
2025-07-01 16:39:48 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,10 +1,10 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 def main_menu(role="user"):
-    kb = [[KeyboardButton("📋 Подать заявку")], [KeyboardButton("📄 Моя анкета")]]
+    kb = [[KeyboardButton(text="📋 Подать заявку")], [KeyboardButton(text="📄 Моя анкета")]]
     if role in ["admin", "superadmin"]:
-        kb.append([KeyboardButton("🛠 Админ-панель")])
-    return ReplyKeyboardMarkup(kb, resize_keyboard=True)
+        kb.append([KeyboardButton(text="🛠 Админ-панель")])
+    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
 
 admin_panel_kb = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="📬 Заявки", callback_data="view_applications")],
