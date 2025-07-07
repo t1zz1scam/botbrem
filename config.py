fef522a7ba5b77a10dce671dc06ea4462441ba84
t1zz1scam <y.selvin24@icloud.com>
2025-07-07 14:23:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")  # PostgreSQL от Render
-SUPER_ADMINS = list(map(int, os.getenv("SUPER_ADMINS", "").split(",")))
-CHANNEL_IDS = list(map(int, os.getenv("CHANNEL_IDS","").split(",")))
+SUPERADMIN_ID = int(os.getenv("SUPERADMIN_ID", "0"))
+SUPER_ADMINS = list(map(int, os.getenv("SUPER_ADMINS", "").split(","))) if os.getenv("SUPER_ADMINS") else []
+CHANNEL_IDS = list(map(int, os.getenv("CHANNEL_IDS", "").split(","))) if os.getenv("CHANNEL_IDS") else []
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
