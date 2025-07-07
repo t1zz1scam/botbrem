84c57c2c3ee99b3f8c8c4dc26901bd82dd5bf631
t1zz1scam <y.selvin24@icloud.com>
2025-07-07 19:53:06 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,7 +2,6 @@ from aiogram import Router
 
 from .profile import router as profile_router
 from .admin import router as admin_router
-# В keyboards.py обычно только клавиатуры, роутера нет, поэтому импортировать не нужно
 
 router = Router()
 router.include_router(profile_router)
