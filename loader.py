8aa872f19c5a2247a7d14eda187af8d65bd880bc
t1zz1scam <y.selvin24@icloud.com>
2025-07-07 19:14:31 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -3,4 +3,4 @@ from aiogram.enums import ParseMode
 from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
-dp = Dispatcher()
+dp = Dispatcher(bot)  # передаём bot в Dispatcher
